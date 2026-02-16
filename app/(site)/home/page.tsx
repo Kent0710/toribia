1df92583544fe9417dae3b82fd5c95344089786f
Kent0710 <christian_kent_bayani@dlsl.edu.ph>
2026-02-16 08:52:10 +0800

    feat: shadcn

---
+++ app/(site)/home/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+    return (
+        <div>
+            <Button>Hello home page</Button>
+        </div>
+    )
+};
+
+export default HomePage;
